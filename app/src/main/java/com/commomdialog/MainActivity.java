5a9d6a70ac77d9fa216eec0cfaf881c4c28681f7
whb <1346788819@qq.com>
2019-04-08 10:05:21 +0800

    自定义dialog

--- app/src/main/java/com/commomdialog/MainActivity.java
+++ app/src/main/java/com/commomdialog/MainActivity.java
@@ -1,13 +1,35 @@
 package com.commomdialog;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button dialog = (Button) findViewById(R.id.new_dialog);
+        dialog.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.new_dialog:
+                new CommomDialog(this, R.style.dialog, "您确定删除此信息？", new CommomDialog.OnCloseListener() {
+                    @Override
+                    public void onClick(Dialog dialog, boolean confirm) {
+                        if(confirm){
+                            Toast.makeText(MainActivity.this,"点击确定", Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        }
+                    }
+                }).setTitle("提示").show();
+        }
     }
 }
